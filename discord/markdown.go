efde89f30047652e7d978ac776c894f3e139d1a3
craftxbox <craftxbox@craftxbox.com>
2022-04-11 12:01:38 -0230

    Update markdown.go

--- discord/markdown.go
+++ discord/markdown.go
@@ -12,9 +12,9 @@ var (
 func ParseMarkdown(md string) string {
 	var res string
 	res = boldRegex.ReplaceAllString(md, "[::b]$1[::-]")
-	res = italicRegex.ReplaceAllString(res, "[::i]$1[::-]")
+	//res = italicRegex.ReplaceAllString(res, "[::i]$1[::-]")
 	res = underlineRegex.ReplaceAllString(res, "[::u]$1[::-]")
-	res = strikeThroughRegex.ReplaceAllString(res, "[::s]$1[::-]")
+	//res = strikeThroughRegex.ReplaceAllString(res, "[::s]$1[::-]")
 
 	return res
 }
