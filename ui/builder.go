b313b9e690718eedc09f69185898ed35c16896da
craftxbox <craftxbox@craftxbox.com>
2022-04-13 11:44:23 -0230

    Update builder.go

--- ui/builder.go
+++ ui/builder.go
@@ -43,6 +43,7 @@ func buildMessage(app *App, m *astatine.Message) []byte {
 				if c != nil &&  len(c.GuildID) > 0 {
 					b.WriteString("cgid")
 					member,_ := app.Session.State.Member(c.GuildID, m.Author.ID) 
+					b.WriteString(_)
 					buildAuthor(&b, m.Author, app.Session.State.User.ID, member,app)
 				} else {
 					b.WriteString("nocgid")
