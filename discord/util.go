612e6373aff8d34eec78a8aed5c66780a58d140b
craftxbox <craftxbox@craftxbox.com>
2022-05-18 11:17:44 -0230

    Update util.go

--- discord/util.go
+++ discord/util.go
@@ -46,12 +46,12 @@ func HasPermission(s *astatine.State, cID string, p int64) bool {
 }
 
 func GetMember(session *astatine.Session, gID string, uID string) *astatine.Member {
-	fmt.println(gID)
-	fmt.println(uID)
+	fmt.Println(gID)
+	fmt.Println(uID)
 	var member *astatine.Member
-	member,err := session.State.Member(gID,uID)
+	member,_ := session.State.Member(gID,uID)
 	if(member == nil){
-		member,err = session.GuildMember(gID,uID)
+		member,_ = session.GuildMember(gID,uID)
 		if(member == nil){
 			return nil
 		}
@@ -63,9 +63,9 @@ func GetMember(session *astatine.Session, gID string, uID string) *astatine.Memb
 
 func GetChannel(session *astatine.Session, cID string) *astatine.Channel {
 	var channel *astatine.Channel
-	channel,err := session.State.Channel(cID)
+	channel,_ := session.State.Channel(cID)
 	if(channel == nil){
-		channel,err = session.Channel(cID)
+		channel,_ = session.Channel(cID)
 		if(channel == nil){
 			return nil
 		}
@@ -77,9 +77,9 @@ func GetChannel(session *astatine.Session, cID string) *astatine.Channel {
 
 func GetGuild(session *astatine.Session, gID string) *astatine.Guild {
 	var guild *astatine.Guild
-	guild,err := session.State.Guild(gID)
+	guild,_ := session.State.Guild(gID)
 	if(guild == nil){
-		guild,err = session.Guild(gID)
+		guild,_ = session.Guild(gID)
 		if(guild == nil){
 			return nil
 		}
