b979e5f92d5c6e2f00b2e0316d7688369ec71d49
craftxbox <craftxbox@craftxbox.com>
2022-05-18 12:16:34 -0230

    Update util.go

--- discord/util.go
+++ discord/util.go
@@ -52,7 +52,7 @@ func GetMember(session *astatine.Session, gID string, uID string) *astatine.Memb
 		if(member == nil){
 			user, _ := session.User(uID) //if user is nil here, something's gone horribly wrong
 			newmem := astatine.Member{GuildID:gID, JoinedAt: time.Time{}, User:user}
-			session.State.MemberAdd(member)
+			session.State.MemberAdd(newmem)
 			return nil
 		}
 		session.State.MemberAdd(member)
